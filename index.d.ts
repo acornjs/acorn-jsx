7fab4ebbc2be22814b968dbdb949eb2a47fa51ec
Nauja <jeremymorosi@hotmail.fr>
2021-06-03 17:45:51 +0200

    Rename AcornJSXOptions to Options in index.d.ts (#124)

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { Parser } from 'acorn' 
 
-declare const jsx: (options?: jsx.AcornJSXOptions) => (BaseParser: typeof Parser) => typeof Parser;
+declare const jsx: (options?: jsx.Options) => (BaseParser: typeof Parser) => typeof Parser;
 
 declare namespace jsx {
   interface Options {
