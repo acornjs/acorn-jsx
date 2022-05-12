1c2fa2c9c98c1195d496ec07baf0053b1ff039d3
Brett Zamir <brettz9@yahoo.com>
2022-05-12 20:51:36 +0800

    fix: use `exports = jsx` (and use jsx.Options)

--- index.d.ts
+++ index.d.ts
@@ -1,62 +1,65 @@
 import * as acorn from 'acorn';
 
-type tokTypes = typeof acorn.tokTypes;
-
-export interface Options {
-  allowNamespacedObjects?: boolean;
-  allowNamespaces?: boolean;
-}
-
-export interface TokTypes extends tokTypes {
-  jsxName: acorn.TokenType,
-  jsxText: acorn.TokenType,
-  jsxTagEnd: acorn.TokenType,
-  jsxTagStart: acorn.TokenType
-}
-
 declare const jsx: {
-  tokTypes: TokTypes;
-  (options?: acorn.Options): (BaseParser: typeof acorn.Parser) => AcornJsxParser
+  tokTypes: typeof acorn.tokTypes;
+  (options?: jsx.Options): (BaseParser: typeof acorn.Parser) => jsx.AcornJsxParser
 }
 
-export type TokContexts = {
-  tc_oTag: acorn.TokContext,
-  tc_cTag: acorn.TokContext,
-  tc_expr: acorn.TokContext
-};
+declare namespace jsx {
 
-type P = typeof acorn.Parser;
+  type tokTypes = typeof acorn.tokTypes;
 
-// We pick (statics) from acorn rather than plain extending to avoid complaint
-//   about base constructors needing the same return type (i.e., we return
-//   `IAcornJsxParser` here)
-export interface AcornJsxParser extends Pick<P, keyof P> {
-  readonly acornJsx: {
-    tokTypes: TokTypes;
-    tokContexts: TokContexts
+  export interface TokTypes extends tokTypes {
+    jsxName: acorn.TokenType,
+    jsxText: acorn.TokenType,
+    jsxTagEnd: acorn.TokenType,
+    jsxTagStart: acorn.TokenType
+  }
+
+  export interface Options {
+    allowNamespacedObjects?: boolean;
+    allowNamespaces?: boolean;
+  }
+
+  export type TokContexts = {
+    tc_oTag: acorn.TokContext,
+    tc_cTag: acorn.TokContext,
+    tc_expr: acorn.TokContext
   };
 
-  new (options: acorn.Options, input: string, startPos?: number): IAcornJsxParser;
+  type P = typeof acorn.Parser;
+
+  // We pick (statics) from acorn rather than plain extending to avoid complaint
+  //   about base constructors needing the same return type (i.e., we return
+  //   `IAcornJsxParser` here)
+  export interface AcornJsxParser extends Pick<P, keyof P> {
+    readonly acornJsx: {
+      tokTypes: TokTypes;
+      tokContexts: TokContexts
+    };
+
+    new (options: acorn.Options, input: string, startPos?: number): IAcornJsxParser;
+  }
+
+  export interface IAcornJsxParser extends acorn.Parser {
+    jsx_readToken(): string;
+    jsx_readNewLine(normalizeCRLF: boolean): void;
+    jsx_readString(quote: number): void;
+    jsx_readEntity(): string;
+    jsx_readWord(): void;
+    jsx_parseIdentifier(): acorn.Node;
+    jsx_parseNamespacedName(): acorn.Node;
+    jsx_parseElementName(): acorn.Node | string;
+    jsx_parseAttributeValue(): acorn.Node;
+    jsx_parseEmptyExpression(): acorn.Node;
+    jsx_parseExpressionContainer(): acorn.Node;
+    jsx_parseAttribute(): acorn.Node;
+    jsx_parseOpeningElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
+    jsx_parseClosingElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
+    jsx_parseElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
+    jsx_parseText(): acorn.Node;
+    jsx_parseElement(): acorn.Node;
+  }
 }
 
-export interface IAcornJsxParser extends acorn.Parser {
-  jsx_readToken(): string;
-  jsx_readNewLine(normalizeCRLF: boolean): void;
-  jsx_readString(quote: number): void;
-  jsx_readEntity(): string;
-  jsx_readWord(): void;
-  jsx_parseIdentifier(): acorn.Node;
-  jsx_parseNamespacedName(): acorn.Node;
-  jsx_parseElementName(): acorn.Node | string;
-  jsx_parseAttributeValue(): acorn.Node;
-  jsx_parseEmptyExpression(): acorn.Node;
-  jsx_parseExpressionContainer(): acorn.Node;
-  jsx_parseAttribute(): acorn.Node;
-  jsx_parseOpeningElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
-  jsx_parseClosingElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
-  jsx_parseElementAt(startPos: number, startLoc?: acorn.SourceLocation): acorn.Node;
-  jsx_parseText(): acorn.Node;
-  jsx_parseElement(): acorn.Node;
-}
-
-export default jsx;
+export = jsx;
